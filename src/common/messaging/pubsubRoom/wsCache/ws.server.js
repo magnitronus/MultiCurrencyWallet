45f0c3737fbcef6ec46f30c8c86e43a50f0392c0
Anton Lyisenkov <anton@lyisenkov.ru>
2021-05-22 08:16:08 +0400

    Draft

--- src/common/messaging/pubsubRoom/wsCache/ws.server.js
+++ src/common/messaging/pubsubRoom/wsCache/ws.server.js
@@ -1,97 +1,78 @@
-import WebSocket from 'ws';
-import {wsMessagesClient, wsMessagesServer} from "./messages";
-const CLIENT_TIMEOUT = 60 * 3;
-
-const wss = new WebSocket.Server({ port: 8080 });
-
-const peers = new Set();
-let orders = [];
-
-const clients = new Set();
-
-const clientsInfo = {}
-
-const broadcast = (message) => {
-    clients.forEach((client) => {
+"use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
+};
+exports.__esModule = true;
+var WebSocket = require("ws");
+var messages_1 = require("./messages");
+var CLIENT_TIMEOUT = 60 * 3;
+var wss = new WebSocket.Server({ port: 8080 });
+var clientsInfo = {};
+var broadcast = function (message) {
+    wss.clients.forEach(function (client) {
         client.send(JSON.stringify(message));
-    })
-}
-
-class MessageHandler {
-
-    handlers = {
-        [wsMessagesClient.PeerConnected]: this._handleNewPeer,
-        [wsMessagesClient.PeerOrdersInit]: this._handlePeerOrders,
-        [wsMessagesClient.Pong]: this._handleClientPong,
+    });
+};
+var MessageHandler = /** @class */ (function () {
+    function MessageHandler() {
+        var _a;
+        var _this = this;
+        this.handlers = (_a = {},
+            _a[messages_1.wsMessagesClient.PeerConnected] = this._handleNewPeer,
+            _a[messages_1.wsMessagesClient.PeerOrdersInit] = this._handlePeerOrders,
+            _a);
+        this.handleMessage = function (client, message) {
+            var data = JSON.parse(message);
+            var handler = _this.handlers[data.type].bind(_this);
+            handler(client, data.data);
+        };
     }
-
-    handleMessage = (client, message) => {
-        const data = JSON.parse(message);
-        const handler = this.handlers[data.type].bind(this)
-        handler(client, data.data)
-    }
-
-    _handleClientPong(client, {peerId}) {
-        const clientInfo = clientsInfo[peerId] || {client};
-        clientInfo.lastSeen = new Date();
-    }
-
-    _handleNewPeer(client, {peerId}) {
-        peers.add(peerId);
-        clientsInfo[peerId] = {client, peerId}
-        this.broadcast({type: wsMessagesClient.PeerConnected, data: {peerId}});
-    }
-
-    _handlePeerOrders(client, {peerId, orders: peerOrders}) {
-        const ids = new Set();
-        // unique orders by id
-        orders = [...orders, ...peerOrders].filter((order) => {
-            if (!ids.has(order.id)) {
-                ids.add(order.id);
-                return true
-            }
-            return false;
-        })
-        broadcast({type: wsMessagesClient.PeerOrdersInit, data: {peerId, orders: peerOrders}});
-    }
-}
-
-const messageHandler = new MessageHandler();
-
+    MessageHandler.prototype._handleNewPeer = function (client, _a) {
+        var peerId = _a.peerId;
+        clientsInfo[client.id].peerId = peerId;
+        broadcast({ type: messages_1.wsMessagesClient.PeerConnected, data: { peerId: peerId } });
+    };
+    MessageHandler.prototype._handlePeerOrders = function (client, _a) {
+        var peerId = _a.peerId, peerOrders = _a.orders;
+        clientsInfo[client.id].orders = peerOrders;
+        broadcast({
+            type: messages_1.wsMessagesClient.PeerOrdersInit,
+            data: { peerId: peerId, orders: peerOrders }
+        });
+    };
+    return MessageHandler;
+}());
+var messageHandler = new MessageHandler();
 wss.on('connection', function connection(ws) {
-  clients.add(ws);
-
-  ws.on('message', function incoming(message) {
-    messageHandler.handleMessage(ws, message)
-  });
-  ws.send(JSON.stringify({type: wsMessagesServer.Peers, data: [...peers]}));
-  ws.send(JSON.stringify({type: wsMessagesServer.Orders, data: [...orders]}));
+    ws.isAlive = true;
+    clientsInfo[ws.id] = { orders: [] };
+    ws.on('message', function incoming(message) {
+        messageHandler.handleMessage(ws, message);
+    });
+    var peers = Object.entries(clientsInfo).reduce(function (acc, _a) {
+        var _ = _a[0], info = _a[1];
+        return __spreadArray(__spreadArray([], acc), [info.peerId]);
+    }, []);
+    var orders = Object.entries(clientsInfo).reduce(function (acc, _a) {
+        var _ = _a[0], info = _a[1];
+        return __spreadArray(__spreadArray([], acc), info.orders);
+    }, []);
+    ws.send(JSON.stringify({ type: messages_1.wsMessagesServer.Peers, data: __spreadArray([], peers) }));
+    ws.send(JSON.stringify({ type: messages_1.wsMessagesServer.Orders, data: __spreadArray([], orders) }));
 });
-
-setInterval(
-    () => {
-        clients.forEach((client) => {
-            client.send(
-                JSON.stringify({type: wsMessagesServer.Ping})
-            )
-        })
-    },
-  30000
-)
-
-setInterval(
-    () => {
-        peers.forEach((peerId) => {
-            const clientInfo = clientsInfo[peerId];
-            if (clientInfo) {
-                const timeDiff = new Date() - clientInfo.lastSeen;
-                if (timeDiff/1000 > CLIENT_TIMEOUT) {
-                    broadcast({type: wsMessagesServer.PeerDisconnected})
-                    clientInfo.client.disconnect();
-                }
-            }
-
-        })
-    },
-  30000
-)
+setInterval(function () {
+    wss.clients.forEach(function (client) {
+        if (client.isAlive === false) {
+            broadcast({
+                type: messages_1.wsMessagesServer.PeerDisconnected,
+                data: { peerId: clientsInfo[client.id].peerId }
+            });
+            delete clientsInfo[client.id];
+            return client.terminate();
+        }
+        client.isAlive = false;
+        client.ping();
+    });
+}, CLIENT_TIMEOUT / 2);
